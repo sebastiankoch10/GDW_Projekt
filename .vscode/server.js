589a2f341cfe49aa552a9fa0a521fdd63b0fe6c4
JonasMaster2K <70891950+JonasMaster2K@users.noreply.github.com>
2022-01-16 14:57:57 +0100

    Update server.js

--- .vscode/server.js
+++ .vscode/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
 console.log("Here")
 res.sendStatus(500).json({message:"Error"})
 })
 
-app.listen(3000)
+app.listen(port)
