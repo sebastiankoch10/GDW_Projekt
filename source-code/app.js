a5fa6043577ece475b46a3f417538ed62cf5ab7c
JonasMaster2K <70891950+JonasMaster2K@users.noreply.github.com>
2022-01-16 17:12:45 +0100

    nodemon

--- source-code/app.js
+++ source-code/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+
+//set up express app
 const app = express();
 const port = process.env.PORT || 3000;
 
+//listen for request
 app.get('/', (req, res) =>{
-    res.send('Hello World!')
+    console.log('GET request');
+    res.send('Hello World!');
+    res.end();
 });
 
 app.listen(port, () =>{
-    console.log('app listening at ${port}')
+    console.log('app listening for requests')
 });
